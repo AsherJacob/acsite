d0bafc12a99520a6d7a6f39ea8a1b06ae1ffc3a5
Nebu Thomas Mathew <themartianmelo@gmail.com>
2020-06-07 22:42:30 +0530

    Homepage done

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
 // =========================== SLIDER ========================================= //
 
-const slides = document.querySelectorAll('.slide');
-const slidesNav = document.querySelectorAll('.slide-circle');
-const auto = true;
-const intervalTime = 3000;
-let slideInterval;
+// const slides = document.querySelectorAll('.slide');
+// const slidesNav = document.querySelectorAll('.slide-circle');
+// const auto = true;
+// const intervalTime = 3000;
+// let slideInterval;
 
-const autoslide = () => {
-    // Get the div with the current class
-    const current = document.querySelector('.current');
-    const active = document.querySelector('.active');
+// const autoslide = () => {
+//     // Get the div with the current class
+//     const current = document.querySelector('.current');
+//     const active = document.querySelector('.active');
 
-    // Remove current class
-    current.classList.remove('current');
-    active.classList.remove('active');
+//     // Remove current class
+//     current.classList.remove('current');
+//     active.classList.remove('active');
 
-    // Check for next element
-    if(current.nextElementSibling){
-        current.nextElementSibling.classList.add('current');
-        active.nextElementSibling.classList.add('active');
-    }
-    else{
-        slides[0].classList.add('current');
-        slidesNav[0].classList.add('active');
-    }
+//     // Check for next element
+//     if(current.nextElementSibling){
+//         current.nextElementSibling.classList.add('current');
+//         active.nextElementSibling.classList.add('active');
+//     }
+//     else{
+//         slides[0].classList.add('current');
+//         slidesNav[0].classList.add('active');
+//     }
 
-    setTimeout(() => {
-        current.classList.remove('current');   
-        active.classList.remove('active');   
-    });
-}
+//     setTimeout(() => {
+//         current.classList.remove('current');   
+//         active.classList.remove('active');   
+//     });
+// }
 
-if(auto){
-    slideInterval = setInterval(autoslide,intervalTime);
-}
+// if(auto){
+//     slideInterval = setInterval(autoslide,intervalTime);
+// }
 
 // ========================== REQUEST FORM MODAL ====================================== //
 
