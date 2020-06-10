dde8796c792d37aae177daa6d4f92adf5bd491d0
AsherJacob <asherjcb@gmail.com>
2020-06-10 09:49:20 +0530

    minor changes

---
+++ site2/form.php
@@ -0,0 +1,33 @@
+        <!-- form processing -->
+        <?php
+        // define variables and set to empty values
+        $fname = $lname = $phone = $email = $message = "";
+        
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $fname = test_input($_POST["fname"]);
+          $lname = test_input($_POST["lname"]);
+          $phone = test_input($_POST["phone"]);
+          $email = test_input($_POST["email"]);
+          $message = test_input($_POST["message"]);
+          
+          $to = "asherjcb@gmail.com";
+          $from = "quotes@mcalinc.ca";
+          $email_subject = "New Quotation" ;
+          $email_body = "You have received a new message ".
+                        " Here are the details:\n First Name: $fname \n Email: $email_address\n Message \n $message";
+          $headers= "From: $from\n";
+          $headers.= "Reply-To: $to";
+          mail($to,$email_subject,$email_body,$headers);
+          ob_clean();
+          header('Location: https://www.mcalinc.ca');
+          exit();
+          
+        }
+        
+        function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
+        ?>
